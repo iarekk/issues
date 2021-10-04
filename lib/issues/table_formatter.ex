61042c363bb8b0103827dee6fa8841309d859e88
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2021-10-04 23:35:02 +0100

    table formatting works

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -2,8 +2,12 @@ defmodule Issues.TableFormatter do
   import Enum, only: [each: 2, map: 2, map_join: 3, max: 1]
 
   def print_table_for_columns(rows, headers) do
-    with data_by_columns = split_into_columns(rows, headers) do
-      IO.inspect(data_by_columns)
+    with data_by_columns = split_into_columns(rows, headers),
+         column_widths = widths_of(headers, data_by_columns),
+         format = format_for(column_widths) do
+      puts_one_line_in_columns(headers, format)
+      IO.puts(separator(column_widths))
+      puts_in_columns(data_by_columns, format)
     end
   end
 
@@ -15,11 +19,35 @@ defmodule Issues.TableFormatter do
     end
   end
 
-  def printable(str) when is_binary(str), do: str
-  def printable(str), do: to_string(str)
+  def widths_of(headers, columns) do
+    cvhs = Enum.zip(headers, columns) |> Enum.map(fn {h, c} -> [h | c] end)
+    for col <- cvhs, do: col |> map(&String.length/1) |> max
+  end
+
+  def format_for(column_widths) do
+    map_join(column_widths, " | ", fn w -> "~-#{w}s" end) <> "~n"
+  end
+
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn w -> List.duplicate("-", w) end)
+  end
+
+  defp printable(str) when is_binary(str), do: str
+  defp printable(str), do: to_string(str)
+
+  def puts_one_line_in_columns(row, format) do
+    :io.format(format, row)
+  end
+
+  def puts_in_columns(data_by_columns, format) do
+    data_by_columns
+    |> List.zip()
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
+  end
 end
 
-# :io.format("~10s", [<<"lol">>])
+# :io.format("~10s", ["lol"])
 #        lol:ok
-# :io.format("~-10s", [<<"lol">>])
+# :io.format("~-10s", ["lol"])
 # lol       :ok
---
+++ test/table_formatter_test.exs
@@ -0,0 +1,43 @@
+defmodule TableFormatterTest do
+  use ExUnit.Case
+  doctest Issues.TableFormatter
+
+  import Issues.TableFormatter,
+    only: [widths_of: 2, split_into_columns: 2, format_for: 1, separator: 1]
+
+  @simple_test_data [
+    [c1: "r1 c1", c2: "r1 c2", c3: "r1 c3", c4: "r1 ++ c4"],
+    [c1: "r2 c1", c2: "r2 c2", c3: "r2 c3", c4: "r2 c4"],
+    [c1: "r3 c1", c2: "r3 c2", c3: "r3 c3", c4: "r3 c4"],
+    [c1: "r4 c1", c2: "r4 long c2", c3: "r4 c3", c4: "r4 c4"]
+  ]
+
+  @headers [:c1, :c2, :c4]
+
+  defp split_in_three_columns do
+    split_into_columns(@simple_test_data, @headers)
+  end
+
+  test "check column splitting" do
+    columns = split_in_three_columns()
+
+    assert columns == [
+             ["r1 c1", "r2 c1", "r3 c1", "r4 c1"],
+             ["r1 c2", "r2 c2", "r3 c2", "r4 long c2"],
+             ["r1 ++ c4", "r2 c4", "r3 c4", "r4 c4"]
+           ]
+  end
+
+  test "test formatting" do
+    assert format_for([2, 5]) == "~-2s | ~-5s~n"
+  end
+
+  test "test separator" do
+    assert separator([2, 5]) == "---+------"
+  end
+
+  test "check the expected lengths" do
+    lens = widths_of(["verylongheader", "h2", "h3"], split_in_three_columns())
+    assert lens == [14, 10, 8]
+  end
+end
