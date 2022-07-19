f3b15a3b5da2242932eae5e20a4b27da599e7b04
Iarek Kovtunenko <iarek.kovtunenko@emailage.com>
2022-07-19 17:45:43 +0100

    add logs

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-agent", "Elixir iarek"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info("Feching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,6 +17,8 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %{status_code: status_code, body: body}}) do
+    Logger.info("Received response with status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
     {
       status_code |> check_for_error,
       body |> Poison.Parser.parse!()
