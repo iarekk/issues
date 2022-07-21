b4d33e40d9fc4214018bf1ddb2e5976d45c45b7d
Iarek Kovtunenko <iarek.kovtunenko@emailage.com>
2022-07-21 20:08:19 +0100

    Introduce code coverage

--- mix.exs
+++ mix.exs
@@ -8,7 +8,14 @@ defmodule Issues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -25,7 +32,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.8.1"},
       {:poison, "~> 5.0.0"},
       {:ex_doc, "~> 0.27"},
-      {:earmark, "~> 1.4.2"}
+      {:earmark, "~> 1.4.2"},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
